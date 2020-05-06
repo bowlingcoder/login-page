7016f972d2d29b02c0ae73e25549fb751689ed57
mattor1000 <ranneyjames90@gmail.com>
2020-05-05 21:07:43 -0400

    working login without consecutive nesting components

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
+import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 
-export default class Login extends React.Component {
+/* Attempt on having a singular component handle both forms 
+   Current issue: Keeping the same state when switching between login and register causing info to bleed
+*/
+export default class BaseForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      email: "",
-      password: "",
-    }
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -21,12 +20,18 @@ export default class Login extends React.Component {
     }
   }
 
+  handlePassword(event){
+    if (event.target.name === "password") {
+      return (event.target.value)
+    }
+  }
+
   render() {
     const error = (this.props.attempts) ? "Incorrect email and password" : " "
     return (
       <div className="page">
-        <Form className="base-form">
-          
+        <Form className="base-form" onSubmit={() => this.props.onClick(this.state.email,this.state.password)}>
+
           <Form.Group>
             <Form.Label>Email:</Form.Label>
             <Form.Control type="email" name="email" onChange={this.handleChange}></Form.Control>
@@ -39,9 +44,8 @@ export default class Login extends React.Component {
 
           <Form.Text className="text-muted">{error}</Form.Text>
 
-          <Button className="page-button" variant="outline-primary" type="button"
-            onClick={() => this.props.onClick(this.state.email, this.state.password)}>Login</Button>
-
+          <Button className="page-button" variant="outline-primary" type="button" 
+            onClick={() => this.props.onClick(this.state.email, this.state.password)}>Submit</Button>
         </Form>
       </div>
     );
